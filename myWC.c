6e08f206a5f563e7411499dc2ae6e89ca80966d6
Pa1em0n <plmn.c.c@gmail.com>
2015-04-12 17:21:26 -0500

    Nuevo commit

--- myWC.c
+++ myWC.c
@@ -9,30 +9,19 @@ void contador(FILE *f){
   char espacio = ' ';
   char saltoLinea = '\n';
   char caracter;
-  //char c = fgetc(f);
-  //int leer[0];
-  //lectura adelantada  
-  // printf("esto es un cppparacter: %c,%d\n",fgetc(f),isspace(fgetc(f))); 
-  //char c = fgetc(f);
   
   while(!feof(f)){
     caracter = fgetc(f);
     if(isspace(caracter) !=0||(caracter == saltoLinea)){
 	pcont ++;
-	//printf(" %c\n",caracter);
       }
     if(isspace(caracter) == 0){
       ccont ++;
-      //printf(" %c\n",caracter);
+      
     }
     if(caracter == saltoLinea){
       lcont ++;
-      //printf(" %c\n",caracter);
     }
-      // printf("---esto es un caracter: %c, %d\n",fgetc(f),isspace(fgetc(f)));   
-      //if(isspace(fgetc(f)) != 0){
-      //printf("esto es un caracter: %c,%d\n",fgetc(f),isspace(fgetc(f)));    
-      //} 
   }
 
   printf(" Numero de palabras: %d\n Numero de caracteres: %d\n Numero de lineas: %d\n",pcont,ccont-1,lcont);
